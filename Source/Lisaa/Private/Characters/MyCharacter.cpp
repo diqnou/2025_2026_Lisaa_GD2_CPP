f40183b319de6c62d1956dddaabf4e75210f26ad
Theo <theo.denanassy@epitech.eu>
2025-11-10 13:01:51 +0100

    adding mapping action & context

--- Source/Lisaa/Private/Characters/MyCharacter.cpp
+++ Source/Lisaa/Private/Characters/MyCharacter.cpp
@@ -1,4 +1,7 @@
 #include "Characters/MyCharacter.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 AMyCharacter::AMyCharacter()
@@ -10,13 +13,34 @@ AMyCharacter::AMyCharacter()
 	SpringArm->TargetArmLength = 350.f;
 	SpringArm->bUsePawnControlRotation = true;
 
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+	Camera->bUsePawnControlRotation = false;
+
 }
 
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (ULocalPlayer* LP = PC->GetLocalPlayer())
+		{
+			if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+				LP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+			{
+				if (IMC_Player)
+				{
+					Subsystem->AddMappingContext(IMC_Player, 0);
+				}
+			}
+		}
+	}
 	
 }
+
+
 
 void AMyCharacter::Tick(float DeltaTime)
 {
@@ -30,3 +54,12 @@ void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 
 }
 
+void AMyCharacter::Move(const FInputActionValue& Value)
+{
+	
+}
+
+void AMyCharacter::Look(const FInputActionValue& Value)
+{
+	
+}
--- Source/Lisaa/Public/Characters/MyCharacter.h
+++ Source/Lisaa/Public/Characters/MyCharacter.h
@@ -2,11 +2,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 
 #include "MyCharacter.generated.h"
 
 class USpringArmComponent;
 class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class LISAA_API AMyCharacter : public ACharacter
@@ -21,11 +24,33 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera");
+	// ************** CAMERA + SPRING ARM **************
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	USpringArmComponent* SpringArm;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera");
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	UCameraComponent* Camera;
+
+	// ************** INPUT **************
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputMappingContext* IMC_Player;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* IA_MyMove;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* IA_MyLook;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* IA_MyJump;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
+
+	
 
 private:
 	UPROPERTY(EditAnywhere, Category = "Movement", meta = (AllowPrivateAccess = "true"))
