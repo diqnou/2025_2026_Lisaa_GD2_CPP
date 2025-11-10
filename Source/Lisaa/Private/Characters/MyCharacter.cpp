1b2b5e05d2964e247283f84a977db98c1b9dc5f6
Theo <theo.denanassy@epitech.eu>
2025-11-10 15:32:05 +0100

    Movement + camera

--- Source/Lisaa/Private/Characters/MyCharacter.cpp
+++ Source/Lisaa/Private/Characters/MyCharacter.cpp
@@ -1,7 +1,9 @@
 #include "Characters/MyCharacter.h"
 
+#include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 
 AMyCharacter::AMyCharacter()
@@ -16,6 +18,9 @@ AMyCharacter::AMyCharacter()
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->SetupAttachment(SpringArm);
 	Camera->bUsePawnControlRotation = false;
+
+	bUseControllerRotationYaw = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 
 }
 
@@ -52,14 +57,36 @@ void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (IA_MyMove)
+			EIC->BindAction(IA_MyMove, ETriggerEvent::Triggered, this, &AMyCharacter::Move);
+		if (IA_MyLook)
+			EIC->BindAction(IA_MyLook, ETriggerEvent::Triggered, this, &AMyCharacter::Look);
+		if (IA_MyJump)
+			EIC->BindAction(IA_MyJump, ETriggerEvent::Started, this, &AMyCharacter::Jump);
+	}
+
 }
 
 void AMyCharacter::Move(const FInputActionValue& Value)
 {
-	
+	const FVector2D Axis = Value.Get<FVector2d>();
+
+	if (Controller && (Axis.Size() > 0.f))
+	{
+		const FRotator YawRot(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Forward = FRotationMatrix(YawRot).GetUnitAxis(EAxis::X);
+		const FVector Right = FRotationMatrix(YawRot).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(Forward, Axis.Y);
+		AddMovementInput(Right, Axis.X);
+	}
 }
 
 void AMyCharacter::Look(const FInputActionValue& Value)
 {
-	
+	const FVector2D Axis = Value.Get<FVector2d>();
+	AddControllerYawInput(Axis.X);
+	AddControllerPitchInput(Axis.Y);
 }
